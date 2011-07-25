52cfe172832410bf9bbdd9206aa4137e44abf24e
Nathan Bowser <nate@nathanbowser.com>
2011-07-25 15:31:28 -0400

    Add example file

---
+++ example/example.clj
@@ -0,0 +1,15 @@
+{"06/29/2011"
+	{:time "3:19'42"
+	:distance 52.52
+	:average 15.7
+	:max 26.7}
+"07/05/2011"
+	{:time "1:29'06"
+	:distance 23.66
+	:average 15.9
+	:max 34.2}
+"07/07/2011"
+	{:time "3:12'34"
+	:distance 45.0
+	:average 15.5
+	:max 30.5}}
