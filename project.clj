1531b1f9a3916d8892df1bdea02398c151372c4a
Nathan Bowser <nate@nathanbowser.com>
2011-07-25 16:55:50 -0400

    Adds the main instruction

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject velostat "0.1.0-SNAPSHOT"
   :description "Simple cycling statics that is accessed from the command-line."
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :main velostat.core)
