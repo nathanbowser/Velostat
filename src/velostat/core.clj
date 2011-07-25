26d2e84c129e23880164d5a6949730ba7a029fb1
Nathan Bowser <nate@nathanbowser.com>
2011-07-25 16:55:12 -0400

    Adds starter code to calculate total milage and average speed

--- src/velostat/core.clj
+++ src/velostat/core.clj
@@ -1,1 +1,13 @@
 (ns velostat.core)
+
+(def a #(reduce (fn [[t c] val] [(+ t val) (inc c)]) [0 0] %))
+
+(defn total-mileage [d]
+	(reduce + (map :distance (vals d))))
+
+(defn average-speed [d]
+	(let [[t c] (a (map :distance (vals d)))] (/ t c)))
+
+(defn -main [& args]
+	(println (total-mileage (read-string (slurp (first args)))))
+	(println (average-speed (read-string (slurp (first args))))))
